0e71e4e2c9e9855f2387f4547cc645931c68b7e3
Resister-boy <hassanpumped17@gmail.com>
2023-11-07 00:32:34 +0900

    Add: DynamiPool Contract Set

---
+++ proto.v2-contract/contracts/dynamic/DynamicPool.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { IDynamicPool } from "./IDynamicPool.sol";
+
+abstract contract DynamicPool is IDynamicPool {
+
+}
---
+++ proto.v2-contract/contracts/dynamic/factory/DynamicPoolFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { IDynamicPoolFactory } from "./IDynamicPoolFactory.sol";
+
+contract DynamicPoolFactory is IDynamicPoolFactory {
+
+}
