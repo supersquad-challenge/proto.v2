3e4506e89cc7dd2241fb57852d8572c0f90dbf86
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-11-14 11:43:42 +0900

    Add: Login Block

---
+++ proto.v2-client/src/components/common/home/LoginBlock.tsx
@@ -0,0 +1,55 @@
+import LongBlueButton from "@/components/base/Button/LongBlueButton";
+import colors from "@/styles/color";
+import styled from "styled-components";
+
+const LoginBlock = () => {
+  return (
+    <BlockWrapper>
+      <Wrapper>
+        <Title>Join SuperSquad now!</Title>
+        <Detail>You can participate in various challenges</Detail>
+        <LongBlueButton
+          margin="15px 0 0 0"
+          title="Login"
+          onClickHandler={() => {}}
+        />
+      </Wrapper>
+    </BlockWrapper>
+  );
+};
+
+export default LoginBlock;
+
+const BlockWrapper = styled.div`
+  width: 100%;
+  height: 193px;
+  background-color: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(2px);
+
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  border-radius: 20px;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  color: ${colors.black};
+  text-align: center;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const Detail = styled.div`
+  color: ${colors.black};
+  text-align: center;
+  font-size: 14px;
+  font-weight: 400;
+`;
