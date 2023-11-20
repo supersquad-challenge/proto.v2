7a7443740eeff615deb405b877b8aee8bb084ce6
sjlee80 <sjlee80@gmail.com>
2023-11-20 12:26:13 +0900

    Fix: add isPhotoUploadedToday

--- proto.v2-server/routes/index.js
+++ proto.v2-server/routes/index.js
@@ -7,6 +7,7 @@ const tx = require('./tx.route');
 const verify = require('./verify.route');
 const myChallenge = require('./myChallenge.route');
 const auth = require('./auth.route');
+const contract = require('./contract.route');
 
 router.use('/user', user);
 router.use('/challenge', challenge);
@@ -14,5 +15,6 @@ router.use('/myChallenge', myChallenge);
 router.use('/tx', tx);
 router.use('/verify', verify);
 router.use('/auth', auth);
+router.use('/contract', contract);
 
 module.exports = router;
