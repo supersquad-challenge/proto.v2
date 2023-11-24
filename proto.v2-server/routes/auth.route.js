7ab46e4390e208abc3f0da765d15fa2d3a0fcfed
sjlee80 <sjlee80@gmail.com>
2023-11-24 13:09:10 +0900

    Fix: google oauth redirect

--- proto.v2-server/routes/auth.route.js
+++ proto.v2-server/routes/auth.route.js
@@ -17,6 +17,7 @@ router.get('/google/callback', (req, res, next) => {
   })(req, res, () => {
     const userInfo = User.findOne({ googleId: req.user.googleId });
 
+    console.log(userInfo);
     if (userInfo.nickname) {
       // res.redirect('http://localhost:3000');
       res.redirect('https://proto-v2-client.vercel.app');
