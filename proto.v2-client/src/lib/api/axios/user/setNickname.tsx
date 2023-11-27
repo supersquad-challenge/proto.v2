e17f16d4a954e6e96fc5914df781db1b46d5c261
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-11-27 11:28:43 +0900

    Fix: urls

--- proto.v2-client/src/lib/api/axios/user/setNickname.tsx
+++ proto.v2-client/src/lib/api/axios/user/setNickname.tsx
@@ -8,8 +8,8 @@ type Props = {
 export const setNickname = async ({ userInfoId, nickname }: Props) => {
   try {
     const res = await axios.post(
-      // `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/nickname`,
-      `http://localhost:8080/user/nickname`,
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/nickname`,
+      // `http://localhost:8080/user/nickname`,
       {
         userId: userInfoId,
         nickname: nickname,
