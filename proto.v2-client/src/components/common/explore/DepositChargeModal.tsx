8f3cb006ef7f8671425e5811ec6e01370ef34adf
Resister-boy <hassanpumped17@gmail.com>
2023-12-01 01:14:45 +0900

    Add: sample tx

--- proto.v2-client/src/components/common/explore/DepositChargeModal.tsx
+++ proto.v2-client/src/components/common/explore/DepositChargeModal.tsx
@@ -143,10 +143,10 @@ const DepositChargeModal = ({
         <SliderNumbersWrapper>
           <SliderNumber>10</SliderNumber>
           <SliderNumber>300</SliderNumber>
-          <div>{JSON.stringify(data)}</div>
-          <div>{isLoading}</div>
-          <div>{isIdle}</div>
-          <div>{isSuccess}</div>
+          <div>{`data: ${JSON.stringify(data)}`}</div>
+          <div>{`loading: ${isLoading}`}</div>
+          <div>{`isIdel: ${isIdle}`}</div>
+          <div>{`success: ${isSuccess}`}</div>
         </SliderNumbersWrapper>
       </SliderContainer>
       <DepositContainer>
